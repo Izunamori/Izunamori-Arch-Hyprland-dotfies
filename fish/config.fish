f4025951f313f9b55bcbb4a2103fa33d3e9eb8ea
Izunamori <158246362+Izunamori@users.noreply.github.com>
2025-04-18 22:57:58 +0500

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_prompt
+    echo -n (prompt_pwd) "➼  " # ➜ ➤ ➦ ➥ ➔ ❯ ❱ ➭ ➙ ➽ ➼
+end
+
+set -U fish_greeting ""
